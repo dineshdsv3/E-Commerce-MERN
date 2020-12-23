abbc5a6faa863a6f5da5b9dead9cda390e3bb5e6
dineshdsv3 <dineshdsv3@gmail.com>
2020-12-23 13:43:12 +0530

    QtySelect

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,7 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import { Row, Col, Image, ListGroup, Card, Button, Form } from 'react-bootstrap';
 import Rating from '../components/Rating';
 import { listProductDetails } from '../actions/productActions';
 import Loader from '../components/Loader';
@@ -10,7 +10,7 @@ import Message from '../components/Message';
 const ProductScreen = ({ match }) => {
 	// const product = products.find((p) => p._id === match.params.id);
 	// const [product, setProduct] = useState([]);
-
+	const [qty, setQty] = useState(0);
 	const dispatch = useDispatch();
 	const productDetails = useSelector((state) => state.productDetails);
 	const { loading, product, error } = productDetails;
@@ -63,6 +63,28 @@ const ProductScreen = ({ match }) => {
 										<Col>{product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}</Col>
 									</Row>
 								</ListGroup.Item>
+								{product.countInStock > 0 && (
+									<ListGroup.Item>
+										<Row>
+											<Col>Qty</Col>
+											<Col>
+												<Form.Control
+													as="select"
+													value={qty}
+													onChange={(e) => {
+														setQty(e.target.value);
+													}}
+												>
+													{[...Array(product.countInStock).keys()].map((x) => (
+														<option key={x + 1} value={x + 1}>
+															{x + 1}
+														</option>
+													))}
+												</Form.Control>
+											</Col>
+										</Row>
+									</ListGroup.Item>
+								)}
 								<ListGroup.Item>
 									<Button className="btn-block" type="button" disabled={product.countInStock === 0}>
 										Add To Cart
