3ab5561d40b70a91673c578ed1ac5533d9097dca
dineshdsv3 <dineshdsv3@gmail.com>
2021-01-04 18:09:18 +0530

    Registration page setup

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -1,4 +1,12 @@
-import { USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAIL, USER_LOGOUT } from '../constants/userConstants.js';
+import {
+	USER_LOGIN_REQUEST,
+	USER_LOGIN_SUCCESS,
+	USER_LOGIN_FAIL,
+	USER_LOGOUT,
+	USER_REGISTER_REQUEST,
+	USER_REGISTER_SUCCESS,
+	USER_REGISTER_FAIL,
+} from '../constants/userConstants.js';
 
 export const userLoginReducer = (state = {}, action) => {
 	switch (action.type) {
@@ -16,6 +24,26 @@ export const userLoginReducer = (state = {}, action) => {
 			};
 		case USER_LOGOUT:
 			return {};
+		default:
+			return state;
+	}
+};
+
+export const userRegisterReducer = (state = {}, action) => {
+	switch (action.type) {
+		case USER_REGISTER_REQUEST:
+			return { loading: true };
+		case USER_REGISTER_SUCCESS:
+			return {
+				loading: false,
+				userInfo: action.payload,
+			};
+		case USER_REGISTER_FAIL:
+			return {
+				loading: false,
+				error: action.payload,
+			};
+
 		default:
 			return state;
 	}
