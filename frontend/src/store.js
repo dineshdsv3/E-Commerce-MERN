7dc5dab1f721aa1a52856454ce4d4b8471db6364
dineshdsv3 <dineshdsv3@gmail.com>
2021-01-13 22:54:12 +0530

    Frontend - Place Order

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,6 +4,7 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import { productListReducer, productDetailsReducer } from './reducers/productReducers.js';
 import { cartReducer } from './reducers/cartReducers';
 import { userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateReducer } from './reducers/userReducers';
+import { orderCreateReducer } from './reducers/orderReducer.js';
 
 const reducer = combineReducers({
 	productList: productListReducer,
@@ -13,6 +14,7 @@ const reducer = combineReducers({
 	userRegister: userRegisterReducer,
 	userDetails: userDetailsReducer,
 	userUpdate: userUpdateReducer,
+	orderCreate: orderCreateReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : [];
